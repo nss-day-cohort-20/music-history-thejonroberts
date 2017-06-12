c49d7e4dec8fb5484a38351a7787206b1f3bd74b
thejonroberts <jon.roberts.8@gmail.com>
2017-06-12 15:06:27 -0500

    input song info into json file

--- songs.js
+++ songs.js
@@ -17,16 +17,10 @@
 
 //Declare array songs - from given file
 var songs = [];
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-//add song to beginning of array songs
-songs.unshift("Capri Pants > by Bikini Kill on the album Reject All American");
-//add song to end of array songs
-songs.push("I am a Scientist > by Guided by Voices on the album Bee Thousand");
+
 // console.log(songs);
+
+
 
 //Declare DOM html element for song list:
 var songList = document.getElementById("songs");
@@ -35,13 +29,6 @@ var songList = document.getElementById("songs");
 
 //loop through songs array to put songs list into html:
 for (var i = 0; i < songs.length; i++) {
-	//remove replace special characters (with proper html tags where needed)
-	songs[i] = songs[i].replace(">", "</h1><p>");
-	songs[i] = songs[i].replace("*", "");
-	songs[i] = songs[i].replace("@", "a");
-	songs[i] = songs[i].replace("(", "");
-	songs[i] = songs[i].replace("&", "&amp;");
-	songs[i] = songs[i].replace("!", "");
 	//write songs to DOM:
 	songList.innerHTML += `<li><h1>${songs[i]}</p></li>`;
 }
