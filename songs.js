26802ed20a116c8b7457c28dc1ec0cc2c51ca1c8
thejonroberts <jon.roberts.8@gmail.com>
2017-06-25 16:55:25 -0500

    file cleanup

--- songs.js
+++ songs.js
@@ -6,33 +6,42 @@ function dataRequestFail() {
 }
 
 function parseSongData() {
-	console.log('getSongData', getSongData);  //these two lines are the same.  myRequest is the event.target
-	console.log('event.target', event.target);
 	var data = JSON.parse(event.target.responseText);  //parse the returned info into js object
-	console.log('data', data);
+	// console.log('data', data);
 	outputSongs(data.song);
 }
 
-//set up event listeners for compeeted request and aborted request
-getSongData.addEventListener("load", parseSongData);
-getSongData.addEventListener("error", dataRequestFail);
-
-//GET, POST, PUT, DELETE, PATCH - main methods (http verb) for xmr
-//tell it which http ver to use
-getSongData.open("GET", "music.json");
-//go get it, boy!
-getSongData.send();
 
 function outputSongs(songsArray) {
 	let songList = document.getElementById("songList");
 	songsArray.forEach(function(song) {
-		songList.innerHTML += `<li><button>Remove</button><span class="title">${song.title} </span><span> by ${song.artist} from the album ${song.album}</span></li>`;
+		songList.innerHTML += `<li class="songListItem"><button class="hideButton">Remove</button><span class="title">${song.title} </span><span> by ${song.artist} from the album ${song.album}</span></li>`;
 	});
 }
-//loop through songs array to put songs list into html:
-for (var i = 0; i < songs.length; i++) {
-	//write songs to DOM:
-	songList.innerHTML += `<li><h1>${songs[i]}</p></li>`;
+
+let hideButtons = document.getElementsByClassName("hideButton");
+console.log('hideButtons', hideButtons);
+let songListIem = document.getElementsByClassName("songListItem");
+
+function addHideClass(i) {
+		console.log('classhide', songListIem[i]);
+	};
+
+for (var i = 0; i < hideButtons.length; i++) {
+	hideButtons[i].addEventListener("click", addHideClass);
+	console.log('buttons added?');
 }
-// console.log(songs);
-// console.log(songList);
+
+//set up event listeners for completed request and aborted request
+getSongData.addEventListener("load", parseSongData);
+getSongData.addEventListener("error", dataRequestFail);
+
+//tell it which http ver to use 
+getSongData.open("GET", "music.json");
+getSongData.send();
+
+
+
+
+
+
