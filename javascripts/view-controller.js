fa1a679e64c403bbeb32b6a07a5ced4954f8b30c
thejonroberts <jon.roberts.8@gmail.com>
2017-07-27 20:47:27 -0500

    wrap view-controller listeners and call them in page switching

--- javascripts/view-controller.js
+++ javascripts/view-controller.js
@@ -6,29 +6,31 @@ let templates = require('./template-builder');
 let factory = require('./song-factory');
 let filter = require('./song-filter');
 
-//TODO - INJECT ADD AND VIEW TEMPLATES ON LOAD
+//run on load:
+showSongList();
+songController.songsToDOM();
+
 function showSongList() {
 	$('#pageWrapper').html( templates.songListView() );
 }
 
-showSongList();
-songController.songsToDOM();
-
-//hide all wrappers, show desired wrapper via id (css .hidden class)
 function showAddSong() {
 	$('#pageWrapper').html( templates.addSongView() );
 }
-//listeners for view links
+
+//nav links - inject templates and start listeners
 $("#addMusicAnchor").click( function() {
 	showAddSong();
+	addSongFormHandler();
 });
 
 $("#listMusicAnchor").click( function() {
 	showSongList();
 	songController.songsToDOM();
+	viewSelectFilterHandlers();
 });
-//song add form handling
-//notify user that click does something:
+
+//notify user that add song click does something:
 function userAddNotification(newSong) {
 	//remove any previous notification
 	$("#addNotification").empty();
@@ -36,19 +38,22 @@ function userAddNotification(newSong) {
 	$("#submitSongAdd").after(addNotification);
 }
 
-$("#addSongForm").submit( function() {
-	//build new song object from user input
-	let songObject = {};
-	songObject.title = $("#titleEntry").val();
-	songObject.artist = $("#artistEntry").val();
-	songObject.album = $("#albumEntry").val();
-	//add Song to stored array in factory module
-	factory.addSong(songObject);
-	//notify user of add
-	userAddNotification(songObject);
-});
+function addSongFormHandler() {
+	$("#addSongForm").submit( function() {
+		//build new song object from user input
+		let songObject = {};
+		songObject.title = $("#titleEntry").val();
+		songObject.artist = $("#artistEntry").val();
+		songObject.album = $("#albumEntry").val();
+		//add Song to stored array in factory module
+		factory.addSong(songObject);
+		//notify user of add
+		userAddNotification(songObject);
+	});
+}
 
 //listeners for select dropdowns; call fns in song-filter.js
+function viewSelectFilterHandlers() {
 $(document).on('change', '#artist-select', function() {
 	filter.filterArtist( $('#artist-select').val() );
 });
@@ -56,6 +61,7 @@ $(document).on('change', '#artist-select', function() {
 $(document).on('change', '#album-select', function() {
 	filter.filterAlbum( $('#album-select').val() );
 });
+}
 
 
 
